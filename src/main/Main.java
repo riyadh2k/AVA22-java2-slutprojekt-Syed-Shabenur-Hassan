06b4c398730d62fbeebc5454e0e4cec23da8a477
Syed Shabenur Hassan <112406985+riyadh2k@users.noreply.github.com>
2023-11-03 22:46:19 +0100

    test3

---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import controller.ProductionController;
+
+
+public class Main {
+    public static void main(String[] args) {
+        new ProductionController().start();
+
+
+    }
+}
